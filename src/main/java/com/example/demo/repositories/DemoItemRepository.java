c332ef8ecae0351ec1f9dd3e4d42958abe9d8493
Ayaka Niiyama <a.niiyama@imagine-labo.co.jp>
2025-08-06 14:48:56 +0900

    javaファイルの追記修正

--- src/main/java/com/example/demo/repositories/DemoItemRepository.java
+++ src/main/java/com/example/demo/repositories/DemoItemRepository.java
@@ -1,11 +1,19 @@
 package com.example.demo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.DemoItem;
 
 @Repository
 public interface DemoItemRepository extends JpaRepository<DemoItem, Long> {
-    
+
+    @Query("SELECT d FROM DemoItem d WHERE "
+    +"(:brandId IS NULL OR :brandId = 0 OR d.brand.brandId = :brandId) AND "
+    +"(:keyword IS NULL OR d.demoName LIKE %:keyword%)")
+    List<DemoItem> searchByConditions(@Param("brandId") Long brandId, @Param("keyword") String keyword);
 }
